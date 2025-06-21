b7b3a56c2ae59549225e9c09d07c9458eb6e81e2
Markus Ressel <mail@markusressel.de>
2025-06-21 19:11:56 +0200

    add github files

--- create_wheel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# install wheel if not done already
-pip install wheel
-# delete old dist packages if any
-rm -rf ./dist/*
-# build new packages if any
-python setup.py bdist_wheel --universal
--- upload_pypi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pip install twine
-twine upload dist/*
