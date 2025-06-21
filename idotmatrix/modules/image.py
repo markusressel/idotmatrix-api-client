a8c73632d7a312d9e4985923decb66befffee283
Markus Ressel <mail@markusressel.de>
2025-06-21 02:20:40 +0200

    refactor

--- idotmatrix/modules/image.py
+++ idotmatrix/modules/image.py
@@ -37,31 +37,117 @@ class Image:
             self.logging.error(f"could not enter image mode due to {error}")
             return False
 
-    @staticmethod
-    def _split_into_chunks(data: bytearray | bytes, chunk_size: int) -> List[bytearray]:
-        """Split the data into chunks of specified size.
+    async def upload_image_file(
+        self,
+        file_path: PathLike,
+        pixel_size: int = 64
+    ) -> None:
+        """Uploads a file processed and makes sure everything is correct before uploading to the device.
 
         Args:
-            data (bytearray): data to split into chunks
-            chunk_size (int): size of the chunks
-
-        Returns:
-            List[bytearray]: returns list with chunks of given data input
+            file_path (str): path-like object to the image file
+            pixel_size (int, optional): number of pixels (one of 16, 32 or 64). Defaults to 64.
         """
-        return [data[i: i + chunk_size] for i in range(0, len(data), chunk_size)]
+        pixel_data = self._load_image_and_adapt_to_canvas(file_path, pixel_size)
+        return await self._send_diy_image_data(pixel_data)
 
     @staticmethod
-    def _short_to_bytes_le(value):
+    def _load_image_and_adapt_to_canvas(
+        file_path: PathLike,
+        pixel_size: int
+    ) -> bytearray:
+        """
+        Loads an image from a file, resizes it to fit within a square canvas of pixel_size x pixel_size,
+        and applies a black background if the image is smaller than the canvas size.
+        Args:
+            file_path (str): Path to the image file.
+            pixel_size (int): Size of the square canvas in pixels.
+        Returns:
+            bytearray: A bytearray containing the raw pixel data of the processed image in RGB format.
+        """
+        with PilImage.open(file_path) as img:
+            background_color = (0, 0, 0)  # black background
+            # resize image to pixel_size x pixel_size, but keep aspect ratio, and fill with background_color if the image is smaller
+            img.thumbnail((pixel_size, pixel_size), PilImage.Resampling.LANCZOS)
+            new_img = PilImage.new("RGB", (pixel_size, pixel_size), background_color)
+            new_img.paste(
+                img, ((pixel_size - img.width) // 2, (pixel_size - img.height) // 2)
+            )
+            img = new_img
+
+            # Read EXIF orientation tag if exists
+            try:
+                for orientation in ExifTags.TAGS.keys():
+                    if ExifTags.TAGS[orientation] == 'Orientation':
+                        break
+                exif = img._getexif()
+                if exif is not None:
+                    orientation_value = exif.get(orientation, None)
+                    if orientation_value == 3:
+                        img = img.rotate(180, expand=True)
+                    elif orientation_value == 6:
+                        img = img.rotate(270, expand=True)  # rotate 270° == rotate right 90°
+                    elif orientation_value == 8:
+                        img = img.rotate(90, expand=True)  # rotate 90° == rotate left 90°
+            except Exception:
+                pass  # no exif or orientation tag, ignore
+            # Convert to RGB if not already in that mode
+            mode = "RGB"
+            if img.mode != mode:
+                img = img.convert(mode)
+
+            # png_buffer = io.BytesIO()
+            # img.save(png_buffer, format="PNG")
+            # png_buffer.seek(0)
+            png_buffer = bytearray(img.tobytes())
+            return png_buffer
+
+    async def upload_image_pixeldata(
+        self, pixel_data: List[Tuple[int, int, int]], pixel_size: int = 64
+    ) -> None:
+        """
+        Uploads pixel data to the iDotMatrix device.
+        Args:
+            pixel_data (List[Tuple[int, int, int]]): List of tuples representing RGB pixel values.
+                Each tuple should contain three integers (R, G, B) in the range 0-255.
+            pixel_size (int): Size of the square canvas in pixels. Must be one of [16, 32, 64]. Defaults to 64.
+        """
+        if pixel_size not in [16, 32, 64]:
+            raise ValueError("pixel_size must be one of [16, 32, 64]")
+        if len(pixel_data) != pixel_size * pixel_size:
+            raise ValueError(
+                f"pixel_data must contain exactly {pixel_size * pixel_size} pixels (quared pixel_size), got: {len(pixel_data)}"
+            )
+
+        # Convert pixel_data to a bytearray in RGB format
+        pixel_data = bytearray()
+        for r, g, b in pixel_data:
+            if not (0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255):
+                raise ValueError(f"Invalid RGB value: ({r}, {g}, {b}). Values must be between 0 and 255.")
+            pixel_data.extend([r, g, b])
+
+        return await self._send_diy_image_data(pixel_data)
+
+    async def _send_diy_image_data(
+        self, pixel_data: bytearray,
+    ) -> None:
+        packets = self._create_diy_image_data_packets(pixel_data)
+        if self.conn:
+            await self.conn.connect()
+            await self.conn.send_packets(data=packets)
+
+    @staticmethod
+    def _short_to_bytes_le(value: int) -> bytes:
         """Converts a short (2 bytes) to little-endian bytes."""
         return struct.pack('<h', value)
 
     @staticmethod
-    def _int_to_bytes_le(value):
+    def _int_to_bytes_le(value: int) -> bytes:
         """Converts an int (4 bytes) to little-endian bytes."""
         return struct.pack('<i', value)
 
     @staticmethod
-    def chunk_data_by_size(data, chunk_size):
+    def chunk_data_by_size(data: bytearray | bytes, chunk_size: int) -> List[bytearray]:
         """
         Chunks the input data into smaller pieces of a specified size.
 
@@ -154,158 +240,3 @@ class Image:
             send_data_3.append(ble_packets_for_chunk)
 
         return send_data_3
-
-    @staticmethod
-    def create_uniform_color_rgb_byte_array(width, height, color_rgb):
-        """
-        Creates a flat byte array in RGB format for an image of a uniform color.
-
-        Args:
-            width (int): The width of the image.
-            height (int): The height of the image.
-            color_rgb (tuple): An (R, G, B) tuple representing the color to be applied
-                               to all pixels. Each R, G, B value should be an integer
-                               between 0 and 255.
-
-        Returns:
-            bytearray: A bytearray containing the RGB data (R, G, B, R, G, B, ...),
-                       or None if the input is invalid.
-        """
-        if width <= 0 or height <= 0:
-            print("Error: Width and height must be positive integers.")
-            return None
-
-        if not (isinstance(color_rgb, tuple) and len(color_rgb) == 3):
-            print("Error: color_rgb must be a tuple of 3 integers (R, G, B).")
-            return None
-
-        r, g, b = color_rgb
-        if not (0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255):
-            print(f"Error: Invalid RGB color value in color_rgb: ({r}, {g}, {b}). "
-                  "Values must be between 0 and 255.")
-            return None
-
-        num_pixels = width * height
-        # Each pixel is 3 bytes (R, G, B)
-        rgb_byte_array = bytearray(num_pixels * 3)
-
-        output_byte_index = 0
-        for _ in range(num_pixels):
-            rgb_byte_array[output_byte_index] = r
-            output_byte_index += 1
-            rgb_byte_array[output_byte_index] = g
-            output_byte_index += 1
-            rgb_byte_array[output_byte_index] = b
-            output_byte_index += 1
-
-        return rgb_byte_array
-
-    # async def uploadUnprocessed(self, file_path: str) -> Union[bool, bytearray]:
-    #     """Uploads an image without further checks and resizes.
-    #
-    #     Args:
-    #         file_path (str): path to the image file
-    #
-    #     Returns:
-    #         Union[bool, bytearray]: False if there's an error, otherwise returns bytearray payload
-    #     """
-    #     try:
-    #         png_data = self._loadPNG(file_path)
-    #         data = self._createPayloads(png_data)
-    #         if self.conn:
-    #             await self.conn.connect()
-    #             await self.conn.send(data=data)
-    #         return data
-    #     except BaseException as error:
-    #         self.logging.error(f"could not upload the unprocessed image: {error}")
-    #         return False
-
-    async def upload_image_file(
-        self, file_path: PathLike, pixel_size: int = 64
-    ) -> None:
-        """Uploads a file processed and makes sure everything is correct before uploading to the device.
-
-        Args:
-            file_path (str): path-like object to the image file
-            pixel_size (int, optional): number of pixels (one of 16, 32 or 64). Defaults to 64.
-        """
-        pixel_data = self._load_image_and_adapt_to_canvas(file_path, pixel_size)
-        return await self._send_diy_image_data(pixel_data)
-
-    async def upload_image_pixeldata(
-        self, pixel_data: List[Tuple[int, int, int]], pixel_size: int = 64
-    ) -> None:
-        if pixel_size not in [16, 32, 64]:
-            raise ValueError("pixel_size must be one of [16, 32, 64]")
-        if len(pixel_data) != pixel_size * pixel_size:
-            raise ValueError(
-                f"pixel_data must contain exactly {pixel_size * pixel_size} pixels (quared pixel_size), got: {len(pixel_data)}"
-            )
-
-        # Convert pixel_data to a bytearray in RGB format
-        pixel_data = bytearray()
-        for r, g, b in pixel_data:
-            if not (0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255):
-                raise ValueError(f"Invalid RGB value: ({r}, {g}, {b}). Values must be between 0 and 255.")
-            pixel_data.extend([r, g, b])
-
-        return await self._send_diy_image_data(pixel_data)
-
-    async def _send_diy_image_data(
-        self, pixel_data: bytearray,
-    ) -> None:
-        packets = self._create_diy_image_data_packets(pixel_data)
-        if self.conn:
-            await self.conn.connect()
-            await self.conn.send_packets(data=packets)
-
-    @staticmethod
-    def _load_image_and_adapt_to_canvas(
-        file_path: PathLike,
-        pixel_size: int
-    ) -> bytearray:
-        """
-        Loads an image from a file, resizes it to fit within a square canvas of pixel_size x pixel_size,
-        and applies a black background if the image is smaller than the canvas size.
-        Args:
-            file_path (str): Path to the image file.
-            pixel_size (int): Size of the square canvas in pixels.
-        Returns:
-            bytearray: A bytearray containing the raw pixel data of the processed image in RGB format.
-        """
-        with PilImage.open(file_path) as img:
-            background_color = (0, 0, 0)  # black background
-            # resize image to pixel_size x pixel_size, but keep aspect ratio, and fill with background_color if the image is smaller
-            img.thumbnail((pixel_size, pixel_size), PilImage.Resampling.LANCZOS)
-            new_img = PilImage.new("RGB", (pixel_size, pixel_size), background_color)
-            new_img.paste(
-                img, ((pixel_size - img.width) // 2, (pixel_size - img.height) // 2)
-            )
-            img = new_img
-
-            # Read EXIF orientation tag if exists
-            try:
-                for orientation in ExifTags.TAGS.keys():
-                    if ExifTags.TAGS[orientation] == 'Orientation':
-                        break
-                exif = img._getexif()
-                if exif is not None:
-                    orientation_value = exif.get(orientation, None)
-                    if orientation_value == 3:
-                        img = img.rotate(180, expand=True)
-                    elif orientation_value == 6:
-                        img = img.rotate(270, expand=True)  # rotate 270° == rotate right 90°
-                    elif orientation_value == 8:
-                        img = img.rotate(90, expand=True)  # rotate 90° == rotate left 90°
-            except Exception:
-                pass  # no exif or orientation tag, ignore
-            # Convert to RGB if not already in that mode
-            mode = "RGB"
-            if img.mode != mode:
-                img = img.convert(mode)
-
-            # png_buffer = io.BytesIO()
-            # img.save(png_buffer, format="PNG")
-            # png_buffer.seek(0)
-            png_buffer = bytearray(img.tobytes())
-            return png_buffer
