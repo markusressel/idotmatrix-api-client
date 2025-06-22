c720872aa7eae2b7c552b8d3d59104aefe593c9b
Markus Ressel <mail@markusressel.de>
2025-06-22 18:30:31 +0200

    avoid sending the same image over and over

--- idotmatrix/digital_picture_frame.py
+++ idotmatrix/digital_picture_frame.py
@@ -43,7 +43,13 @@ class DigitalPictureFrame:
 
         self._is_in_diy_mode: bool = False
 
-    async def start_slideshow(self, interval: int = 30):
+    def set_interval(self, interval: int):
+        """
+        Sets the interval between two images/GIFs for the slideshow.
+        """
+        self.interval = interval
+
+    async def start_slideshow(self, interval: int = None) -> Task:
         """
         Starts a slideshow of images on the digital picture frame.
 
@@ -51,8 +57,9 @@ class DigitalPictureFrame:
             interval (int): Time in seconds between image changes. Defaults to 5 seconds.
         """
         self.logging.info(f"Starting slideshow with interval: {interval} seconds")
-        self.interval = interval
-        self._slideshow_task = self._start_slideshow_task(interval)
+        if interval is not None:
+            self.set_interval(interval)
+        self._slideshow_task = self._start_slideshow_task()
         return self._slideshow_task
 
     async def stop_slideshow(self):
@@ -66,13 +73,13 @@ class DigitalPictureFrame:
 
         await self.device_client.clock.show()
 
-    def _start_slideshow_task(self, interval) -> Task:
+    def _start_slideshow_task(self) -> Task:
         """
         Starts the slideshow task that uploads images to the device at specified intervals.
         """
-        return asyncio.create_task(self._slideshow_loop(interval))
+        return asyncio.create_task(self._slideshow_loop())
 
-    async def _slideshow_loop(self, interval: int):
+    async def _slideshow_loop(self):
         """
         Internal method to handle the slideshow loop.
         """
@@ -80,35 +87,39 @@ class DigitalPictureFrame:
         await self.device_client.reset()
         while True:
             for image in self.images:
-                if image == self._current_image:
-                    self.logging.info(f"Skipping image '{image}' as it is already beeing displayed currently.")
-                    continue
-                await self._switch_to_next(image)
+                if image != self._current_image:
+                    await self._switch_to_next(image)
+                else:
+                    self.logging.info(f"Skipping image '{image}' as it is already being displayed currently.")
+                await sleep(self.interval)
 
     async def _switch_to_next(self, image: PictureFrameImage | PictureFrameGif | PathLike | str):
         if isinstance(image, PictureFrameImage):
-            await self._set_image(image.file_path)
+            image_path = image.file_path
+            await self._set_image(image_path)
         elif isinstance(image, PictureFrameGif):
+            image_path = image.file_path
             await self._set_gif(
-                file_path=image.file_path,
+                file_path=image_path,
                 duration_per_frame_in_ms=image.duration_per_frame_in_ms
             )
         elif isinstance(image, (PathLike, str)):
             # If it's a string or PathLike, treat it as a file path
             if isinstance(image, PathLike):
-                image = image.__fspath__()
-            if image.lower().endswith('.gif'):
-                await self._set_gif(image)
+                image_path = image.__fspath__()
             else:
-                await self._set_image(image)
+                image_path = image
+            if image_path.lower().endswith('.gif'):
+                await self._set_gif(image_path)
+            else:
+                await self._set_image(image_path)
         else:
             raise ValueError(
                 f"Unsupported image type: {type(image)}. Must be PictureFrameImage, PictureFrameGif, or a file path."
             )
 
         self._current_image = image
-        self.logging.info(f"Displaying image '{image}' for {self.interval} seconds.")
-        await sleep(self.interval)
+        self.logging.info(f"Displaying image '{image_path}' for {self.interval} seconds.")
 
     async def _set_image(
         self,
