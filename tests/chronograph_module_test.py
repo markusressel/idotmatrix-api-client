d570763029d98805533f80f829828a7ac722c6d3
Markus Ressel <mail@markusressel.de>
2025-06-22 19:55:33 +0200

    added test

---
+++ tests/chronograph_module_test.py
@@ -0,0 +1,75 @@
+from unittest.mock import AsyncMock
+
+from idotmatrix.modules.chronograph import ChronographModule
+from tests import TestBase
+
+
+class TestChronographModule(TestBase):
+
+    async def test_reset(self):
+        # GIVEN
+        connection_manager = AsyncMock()
+        under_test = ChronographModule(
+            connection_manager=connection_manager,
+        )
+
+        # WHEN
+        await under_test.reset()
+
+        # THEN
+        connection_manager.send_bytes.assert_awaited_once_with(
+            data=bytearray(b'\x05\x00\t\x80\x00'),
+            response=False,
+            chunk_size=None
+        )
+
+    async def test_start_from_zero(self):
+        # GIVEN
+        connection_manager = AsyncMock()
+        under_test = ChronographModule(
+            connection_manager=connection_manager,
+        )
+
+        # WHEN
+        await under_test.start_from_zero()
+
+        # THEN
+        connection_manager.send_bytes.assert_awaited_once_with(
+            data=bytearray(b'\x05\x00\t\x80\x01'),
+            response=False,
+            chunk_size=None
+        )
+
+    async def test_pause(self):
+        # GIVEN
+        connection_manager = AsyncMock()
+        under_test = ChronographModule(
+            connection_manager=connection_manager,
+        )
+
+        # WHEN
+        await under_test.pause()
+
+        # THEN
+        connection_manager.send_bytes.assert_awaited_once_with(
+            data=bytearray(b'\x05\x00\t\x80\x02'),
+            response=False,
+            chunk_size=None
+        )
+
+    async def test_resume(self):
+        # GIVEN
+        connection_manager = AsyncMock()
+        under_test = ChronographModule(
+            connection_manager=connection_manager,
+        )
+
+        # WHEN
+        await under_test.resume()
+
+        # THEN
+        connection_manager.send_bytes.assert_awaited_once_with(
+            data=bytearray(b'\x05\x00\t\x80\x03'),
+            response=False,
+            chunk_size=None
+        )
