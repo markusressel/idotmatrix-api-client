ffb64895e85db1a0651698efeae0e8d0da9604d7
Markus Ressel <mail@markusressel.de>
2025-06-23 02:59:55 +0200

    added test

---
+++ tests/scoreboard_module_test.py
@@ -0,0 +1,27 @@
+from unittest.mock import AsyncMock
+
+from idotmatrix.modules.scoreboard import ScoreboardModule
+from tests import TestBase
+
+
+class TestScoreboardModule(TestBase):
+
+    async def test_show(self):
+        # GIVEN
+        connection_manager = AsyncMock()
+        under_test = ScoreboardModule(
+            connection_manager=connection_manager,
+        )
+
+        # WHEN
+        await under_test.show(
+            count1=10,
+            count2=25,
+        )
+
+        # THEN
+        connection_manager.send_bytes.assert_awaited_once_with(
+            data=bytearray(b'\x08\x00\n\x80\n\x00\x19\x00'),
+            response=False,
+            chunk_size=None
+        )
